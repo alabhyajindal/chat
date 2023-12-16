3e6a99cce4d1432474d2e1788f1c48f1fea4ee03
Alabhya Jindal <alabhya10@gmail.com>
2023-12-16 13:14:04 +0530

    Creating chat rooms automatically, matching two people in a chat room

--- main.js
+++ main.js
@@ -11,25 +11,40 @@ const io = new Server(server)
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const messages = {
-  football: [],
-  music: [],
-  running: [],
-}
+const rooms = {}
+
+const messages = {}
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'chat.html'))
 })
 
+function findOrCreateRoom() {
+  for (let room in rooms) {
+    if (rooms[room] < 2) {
+      rooms[room]++
+      return room
+    }
+  }
+  const newRoom = nanoid()
+  rooms[newRoom] = 1
+  messages[newRoom] = []
+  return newRoom
+}
+
 io.on('connection', (socket) => {
+  const room = findOrCreateRoom()
+  console.log('file: main.js:38  room', room)
+  socket.emit('find room', room)
+
   socket.on('join room', (roomName) => {
     socket.join(roomName)
   })
 
   socket.on('send message', ({ content, sender, to }) => {
+    console.log('file: main.js:45  to', to)
     if (messages[to]) {
       messages[to].push({ [sender]: content })
-      console.log(messages)
       const payload = { content, sender }
       io.to(to).emit('new message', payload)
     }
@@ -39,3 +54,14 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000')
 })
+
+/*
+A button on the page - click on it:
+  - server checks for existing queues of length 1
+    - if exists, then adds you to one of them
+    - else, creates a new queue
+You then wait in the queue until another person joins the queue
+And then the chat is started
+
+For the queue, using an array would work to test out the concept. And then redis could be used later
+*/
